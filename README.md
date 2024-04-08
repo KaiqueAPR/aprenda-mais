3781f475427e0a4049bd76f187be7a5ba308348e
Kaique Araujo Pinholato Ribeiro <kaiquepinho2010@hotmail.com>
2024-04-08 20:07:40 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# aprenda-mais
+ 
---
+++ src/main/java/com/br/aprendamais/AprendaMaisApplication.java
@@ -0,0 +1,13 @@
+package com.br.aprendamais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendaMaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendaMaisApplication.class, args);
+	}
+
+}
