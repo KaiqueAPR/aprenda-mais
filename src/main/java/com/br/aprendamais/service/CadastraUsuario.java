f0c66259419766b69046577f5b8132046e1e0fa6
Kaique Araujo Pinholato Ribeiro <kaiquepinho2010@hotmail.com>
2024-04-09 21:10:04 -0300

    fix: kkkkkkkk

--- src/main/java/com/br/aprendamais/service/CadastraUsuario.java
+++ src/main/java/com/br/aprendamais/service/CadastraUsuario.java
@@ -1,16 +1,7 @@
 package com.br.aprendamais.service;
 
-import com.br.aprendamais.model.UsuarioModel;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CadastraUsuario {
-
-    public UsuarioModel criaUsuario(UsuarioModel usuarioModel){
-        UsuarioModel novoUsuarioModel = new UsuarioModel();
-        if(usuarioModel != null){
-            novoUsuarioModel = usuarioModel;
-        }
-        return novoUsuarioModel;
-    }
 }
