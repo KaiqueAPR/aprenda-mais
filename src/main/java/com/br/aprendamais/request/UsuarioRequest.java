93bb8ab1525dcf0aef0fa30819ef883bf6d6e8cc
Kaique Araujo Pinholato Ribeiro <kaiquepinho2010@hotmail.com>
2024-04-12 14:51:21 -0300

    fix: change notations

--- src/main/java/com/br/aprendamais/request/UsuarioRequest.java
+++ src/main/java/com/br/aprendamais/request/UsuarioRequest.java
@@ -1,9 +1,6 @@
 package com.br.aprendamais.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Data;
 
@@ -23,14 +20,10 @@ public class UsuarioRequest {
     @Size(max = 14, message = "CPF deve conter no máximo 11 caracteres!")
     private String cpf;
 
-    @NotBlank(message = "Telefone deve ser preenchido!")
     @NotNull(message = "Telefone não pode ser nulo!")
-    @Size(min = 10, max = 11, message = "Telefone deve conter entre 10 e 11 números!")
     private Number telefone;
 
-    @NotBlank(message = "DDD deve ser preenchido!")
     @NotNull(message = "DDD não pode ser nulo!")
-    @Size(max = 2, message = "DDD deve conter 2 números!")
     private Number ddd;
 
     @NotBlank(message = "Email deve ser preenchido!")
@@ -39,13 +32,10 @@ public class UsuarioRequest {
     @Size(max = 255, message = "Email deve ter no máximo 200 caracteres!")
     private String email;
 
-    @NotBlank(message = "Data de Nascimento deve ser preenchido!")
     @NotNull(message = "Data de Nascimento não pode ser nulo!")
     private Date dtNascimento;
 
-    @NotBlank(message = "CEP deve ser preenchido!")
     @NotNull(message = "CEP não pode ser nulo!")
-    @Size(max = 9, message = "CEP deve ter no máximo 9 caracteres!")
     private Number cep;
 
     @NotBlank(message = "Logradouro deve ser preenchido!")
