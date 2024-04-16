ae330896c3ac29ea718822d4ccf62c61fd18af60
= <lyulucca@gmail.com>
2024-04-16 14:54:44 -0300

    feat: routes created

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,17 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+import SignUp from './pages/SignUp/SignUp'
+import Login from './pages/Login/Login'
+
 import {createBrowserRouter, RouterProvider} from "react-router-dom"
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>
+    element: <Login />,
+  },
+  {
+    path: "/cadastro",
+    element: <SignUp />,
   }
-])
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router}/>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
