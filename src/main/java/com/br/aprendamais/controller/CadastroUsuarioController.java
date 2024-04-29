9cb6dcac48b8c1101534f7a9f5a8b3845d4d8d23
douglas.sousa <douglas.sousa@strong.com.br>
2024-04-29 01:37:52 -0300

    add function searchAdress()

--- src/main/java/com/br/aprendamais/controller/CadastroUsuarioController.java
+++ src/main/java/com/br/aprendamais/controller/CadastroUsuarioController.java
@@ -4,13 +4,11 @@ import com.br.aprendamais.request.UsuarioRequest;
 import com.br.aprendamais.response.UsuarioResponse;
 import com.br.aprendamais.service.CadastroUsuario;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuario")
+@CrossOrigin("http://localhost:5173/")
 public class CadastroUsuarioController {
 
     private final CadastroUsuario cadastroUsuarioService;
